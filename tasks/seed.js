6b1ef22212582acc2b2ab2fe5c3adb2983075681
Neel Patel <npate27@stevens.edu>
2017-05-07 01:01:41 -0400

    Add temp emails for users for email scheudler testing

--- tasks/seed.js
+++ tasks/seed.js
@@ -4,6 +4,10 @@ const users = data.users;
 const items = data.items;
 const products = data.products;
 var rossId = 0
+var dateNow = new Date();
+var dateTomorrow = new Date();
+dateTomorrow.setDate(dateTomorrow.getDate() + 1); 
+
 dbConnection().then(db => {
     return db.dropDatabase().then(() => {
         return dbConnection;
@@ -12,43 +16,38 @@ dbConnection().then(db => {
     }).then(() => {
         return products.addProduct("041190571901", "Shoprite Sandwich White Bread", "http://content.shoprite.com/legacy/productimagesroot/DJ/8/164788.jpg");
     }).then(() => {
-        return products.addProduct("016000275638", "General Mills Total Cereal", "http://ecx.images-amazon.com/images/I/51cGjp6uMmL._SL160_.jpg");
+        return products.addProduct("016000275638", "General Mills Total Cereal", "https://i5.walmartimages.com/asr/b0442517-0147-4703-a17b-a8e45684abef_1.2ec464c5751f281255d794398d9af225.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff");
     }).then(() => {
-        return products.addProduct("044000025298", "Nabisco Oreo ", "https://i5.walmartimages.com/asr/b0442517-0147-4703-a17b-a8e45684abef_1.2ec464c5751f281255d794398d9af225.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff");
+        return products.addProduct("044000025298", "Nabisco Oreo ", "http://ecx.images-amazon.com/images/I/51cGjp6uMmL._SL160_.jpg");
     }).then(() => {
         //Pass = "1234"
-        return users.addUser("$2a$04$tO3YCc6S2x7ssKwlm3rOiecoicyYVK/xuBS/0F3MCJ8PQ8uOu4aTW", "1", "Bob Ross", "Happy little plants only", "bobross@pbs.org");
+        return users.addUser("$2a$04$tO3YCc6S2x7ssKwlm3rOiecoicyYVK/xuBS/0F3MCJ8PQ8uOu4aTW", "1", "Bob Ross", "Happy little plants only", "fopahe@lgxscreen.com");
     }).then((bobross) => {
         rossId = bobross._id
         //Pass = "password"
-        return users.addUser("$2a$04$FpQEGLPDC.E.aWyBtB0vlOdy5MLSpVha390795nfmkI71ML2H8Iu6", "2", "Jane Doe", "Gluten free", "janedoe@gmail.com");
+        return users.addUser("$2a$04$FpQEGLPDC.E.aWyBtB0vlOdy5MLSpVha390795nfmkI71ML2H8Iu6", "2", "Jane Doe", "Gluten free", "wibuq@nada.email");
     }).then((janeDoe) => {
         const userId = janeDoe._id;
-        let dateNow = new Date();
         let formatedDate = (dateNow.getMonth() + 1) + "-" + dateNow.getUTCDate() + "-" + dateNow.getFullYear();
-        let expireDate = (dateNow.getMonth() + 1) + "-" + dateNow.getUTCDate() + "-" + dateNow.getFullYear();
+        let expireDate = (dateTomorrow.getMonth() + 1) + "-" + dateTomorrow.getUTCDate() + "-" + dateTomorrow.getFullYear();
         return items.addItem(userId, "Peanut Butter for PB&J", "051500255162", 2, formatedDate, expireDate);
     }).then((itemAdded) => {
         const id = itemAdded.userId;
-        let dateNow = new Date();
         let formatedDate = (dateNow.getMonth() + 1) + "-" + dateNow.getUTCDate() + "-" + dateNow.getFullYear();
-        let expireDate = (dateNow.getMonth() + 1) + "-" + dateNow.getUTCDate() + "-" + dateNow.getFullYear();
+        let expireDate = (dateTomorrow.getMonth() + 1) + "-" + dateTomorrow.getUTCDate() + "-" + dateTomorrow.getFullYear();
         return items.addItem(id, "Bread for PB&J", "041190571901", 1, formatedDate, expireDate);
     }).then((itemAdded) => {
         const id = itemAdded.userId;
-        let dateNow = new Date();
         let formatedDate = (dateNow.getMonth() + 1) + "-" + dateNow.getUTCDate() + "-" + dateNow.getFullYear();
         let expireDate = (dateNow.getMonth() + 1) + "-" + dateNow.getUTCDate() + "-" + dateNow.getFullYear();
         return items.addItem(id, "Total Cereal", "016000275638", 3, formatedDate, expireDate);
     }).then((itemAdded) => {
         const id = itemAdded.userId;
-        let dateNow = new Date();
         let formatedDate = (dateNow.getMonth() + 1) + "-" + dateNow.getUTCDate() + "-" + dateNow.getFullYear();
         let expireDate = (dateNow.getMonth() + 1) + "-" + dateNow.getUTCDate() + "-" + dateNow.getFullYear();
         return items.addItem(id, "Oreos", "044000025298", 9000, formatedDate, expireDate);
     }).then((itemAdded) => {
         const id = rossId;
-        let dateNow = new Date();
         let formatedDate = (dateNow.getMonth() + 1) + "-" + dateNow.getUTCDate() + "-" + dateNow.getFullYear();
         let expireDate = (dateNow.getMonth() + 1) + "-" + dateNow.getUTCDate() + "-" + dateNow.getFullYear();
         return items.addItem(id, "Oreo Cookies", "044000025298", 1, formatedDate, expireDate);
