1c281af4cc95936302544b8ff031909f40848210
Matthew Gomez <magomez96@gmail.com>
2017-05-10 18:09:33 -0400

    Make sure code passes tota11y and minor CSS changes/form validation

--- public/js/products.js
+++ public/js/products.js
@@ -1,19 +1,23 @@
-
-
 function show(ID) {
-    console.log(ID);
+    //console.log(ID);
     var formToShow = document.getElementById("add-" + ID.toString());
     var buttonToHide = document.getElementById("button-" + ID.toString());
-    console.log("add-" + ID.toString())
+    var liToResize = document.getElementById(ID.toString());
+    //console.log("add-" + ID.toString())
     formToShow.classList.remove("hidden");
     buttonToHide.classList.add("hidden");
+    liToResize.classList.remove("itemContainer");
+    liToResize.classList.add("itemContainerBig");
 }
 
 function hide(ID) {
-    console.log(ID);
+    //console.log(ID);
     var formToHide = document.getElementById("add-" + ID.toString());
     var buttonToShow = document.getElementById("button-" + ID.toString());
-    console.log("add-" + ID.toString())
+    var liToResize = document.getElementById(ID.toString());
+    //console.log("add-" + ID.toString())
     formToHide.classList.add("hidden");
     buttonToShow.classList.remove("hidden");
+    liToResize.classList.remove("itemContainerBig");
+    liToResize.classList.add("itemContainer");
 }
