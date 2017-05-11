73ef7b64e032a2b31eaa4530f52b01189037ee59
Neel Patel <npate27@stevens.edu>
2017-05-11 19:00:27 -0400

    Remove print statement from modal JQuery

--- public/js/modal_data.js
+++ public/js/modal_data.js
@@ -5,8 +5,6 @@ $(document).ready(function (e) {
     var recipient = button.data('upc') // Extract info from data-* attributes
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    console.log("TEST")
-    console.log(recipient)
     var modal = $(this)
     modal.find('#upc').val(recipient)
   })
