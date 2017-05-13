9379d9500549b909e0682ae6455db349866f8ca5
Neel Patel <npate27@stevens.edu>
2017-05-13 13:52:08 -0400

    Fix css for dateTomorrow

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -3,10 +3,12 @@ window.onload = function() {
     for (var i = 0; i < items.length; i++) {
         var expDate = new Date(items[i].getAttribute("data-expDate"));
         var daysUntilExpiration = (expDate - new Date()) / (1000 * 60 * 60 * 24);
-
-        if (daysUntilExpiration < 1) {
+        console.log(items[i].nickname);
+        console.log(expDate)
+        console.log(daysUntilExpiration)
+        if (daysUntilExpiration < 1 && daysUntilExpiration < 0) {
             items[i].classList.add("expireToday");
-        } else if (daysUntilExpiration < 2) {
+        } else if (daysUntilExpiration < 2 && daysUntilExpiration > 0) {
             items[i].classList.add("expireTomorrow");
         } else if (daysUntilExpiration < 7) {
             items[i].classList.add("expireThisWeek");
